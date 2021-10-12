cde3b8f7e76cf8d8acfbe529d8fff81115276df9
Talita Vial <talitavialsilva1@gmail.com>
2021-10-11 22:01:19 -0400

    aula React State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+import "./App.css";
 
 const App = () => {
-  return <h1>Hello Word</h1>;
+
+  // let msg = "Hello talita";
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar React',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler Documentação',
+      completed: true,
+    }
+  ]);
+
+  return(
+    <>
+      <div className='container'>
+        <Tasks />
+      </div>; 
+    </>
+  );
+   
 }
 
 export default App;
