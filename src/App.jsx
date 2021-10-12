25f66b5fa0b427885e0f94e34cbc074bcfe0ffd7
Talita Vial <talitavialsilva1@gmail.com>
2021-10-12 15:10:46 -0400

    curso react finalizada as funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import Header from './components/Header';
+import TasksDetails from './components/TasksDetails';
 
 import './App.css';
 
@@ -54,8 +55,13 @@ const App = () => {
     <Router>
       <div className="container">
         <Header />
-        <AddTask handleTaskAddition={handleTaskAddition}/>
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
+        <Route path="/" exact render={() => (
+          <>
+          <AddTask handleTaskAddition={handleTaskAddition}/>
+          <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
+          </>
+        )}/>
+        <Route path="/:taskTitle" exact component={TasksDetails}/>
       </div>
     </Router>
   )
