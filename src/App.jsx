eb987ab75c4a2a37c2258a8ab77ec8e15f35730e
Talita Vial <talitavialsilva1@gmail.com>
2021-10-12 09:38:20 -0400

    aula de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
-import Tasks from './components/Tasks'
-import './App.css'
-import AddTask from './components/AddTask'
+import Tasks from './components/Tasks';
+import './App.css';
+import AddTask from './components/AddTask';
 
 const App = () => {
   // let msg = "Hello talita";
@@ -18,27 +19,36 @@ const App = () => {
       completed: true
     }
     
-  ])
+  ]);
+
+  const handleTaskClick = (taskId) =>{
+      const newTasks = tasks.map((task) => {
+        if (task.id === taskId) return {...task, completed: !task.completed}
+
+        return task;
+      });
+
+      setTasks(newTasks);
+  };
 
   const handleTaskAddition = (taskTitle) =>{
     const newTasks = [...tasks,{
       title: taskTitle,
-      id: Math.random(10),
+      id: uuidv4(),
       completed: false,
-    }]
+    }];
 
     setTasks(newTasks);
-  }
+  };
 
   return (
     <>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition}/>
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
       </div>
-      ;
     </>
   )
-}
+};
 
-export default App
+export default App;
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -11,6 +11,7 @@ const AddTask = ({handleTaskAddition}) => {
 
   const handleAddTaskclick = () => {
     handleTaskAddition(inputData);
+    setInputData('');
   }
 
   return (
